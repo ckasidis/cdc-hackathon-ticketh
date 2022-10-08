8dae384d0ccfb4d68a89fd8c2980e4fceb5378d8
Patipol-BKK <patipol.ti@gmail.com>
2022-10-08 21:52:27 +0800

    add eventsRouter

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,10 +2,12 @@
 import { createRouter } from './context';
 import superjson from 'superjson';
 import { myContractsRouter } from './my-contracts';
+import { eventsRouter } from './events';
 
 export const appRouter = createRouter()
 	.transformer(superjson)
-	.merge('my-contracts.', myContractsRouter);
+	.merge('my-contracts.', myContractsRouter)
+	.merge('events.', eventsRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
