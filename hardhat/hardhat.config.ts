4a2ddf9318cc9091eca7d389dbb468dff7caf014
Rachanon W <ped02@me.com>
2022-10-09 16:29:46 +0800

    Cronos Test Scripts. Need to add privkey to env

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,11 +1,34 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
+
+const getHDWallet = () => {
+	const MNEMONIC = process.env.MNEMONIC!;
+	const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+
+	/*
+	if (MNEMONIC && MNEMONIC !== "") {
+	  return {
+		mnemonic: MNEMONIC,
+	  }
+	}
+	*/
+	if (PRIVATE_KEY && PRIVATE_KEY !== "") {
+	  return [PRIVATE_KEY]
+	}
+	throw Error("Private Key Not Set! Please set up .env");
+}
+
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.17',
 	networks: {
 		hardhat: {
 			chainId: 1337,
+		},
+		cronosTest: {
+			url: "https://evm-t3.cronos.org/",
+      		accounts: getHDWallet(),
 		},
 	},
 };
