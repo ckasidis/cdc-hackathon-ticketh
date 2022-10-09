ba33db94ac35b59794bdaebd49eb43b013883db9
Kasidis Chantharojwong <me@ckasidis.com>
2022-10-09 14:43:53 +0800

    create dashboard profile page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,31 @@
-import { Button, Stack, Text } from '@chakra-ui/react';
+import {
+	Alert,
+	AlertDescription,
+	AlertIcon,
+	Box,
+	Button,
+	Center,
+	HStack,
+	IconButton,
+	Image,
+	Spinner,
+	Stack,
+	Text,
+	useDisclosure,
+	useToast,
+} from '@chakra-ui/react';
+import * as filestack from 'filestack-js';
 import { GetServerSideProps, NextPage } from 'next';
 import { User } from 'next-auth';
 import { signOut } from 'next-auth/react';
 import Head from 'next/head';
+import { FiEdit } from 'react-icons/fi';
+import { useQueryClient } from 'react-query';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
+import UpdateUsernameModal from '../../components/modals/UpdateUsernameModal';
+import { env } from '../../env/client.mjs';
 import { getServerAuthSession } from '../../server/common/get-server-auth-session';
+import { trpc } from '../../utils/trpc';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const session = await getServerAuthSession(ctx);
@@ -29,21 +50,149 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: NextPage<DashboardPageProps> = ({ user }) => {
+	const updateUsername = useDisclosure();
+	const toast = useToast();
+
+	const queryClient = useQueryClient();
+
+	const {
+		data: profile,
+		isLoading,
+		isError,
+	} = trpc.useQuery(['users.read-user', { id: user.id }]);
+
+	const { mutate: updateImage } = trpc.useMutation(
+		'my-profile.update-profile',
+		{
+			onSuccess: () => {
+				queryClient.invalidateQueries('users.read-user');
+				toast({
+					title: 'Profile photo updated successfully',
+					status: 'success',
+					isClosable: true,
+					position: 'bottom-right',
+				});
+			},
+			onError: (error) => {
+				toast({
+					title: error.message,
+					status: 'error',
+					isClosable: true,
+					position: 'bottom-right',
+				});
+			},
+		}
+	);
+
+	const uploadProfileImage = async () => {
+		const client = filestack.init(env.NEXT_PUBLIC_FILESTACK_API_KEY);
+		const options: filestack.PickerOptions = {
+			accept: 'image/*',
+			transformations: {
+				crop: {
+					aspectRatio: 1 / 1,
+				},
+			},
+			onFileUploadFinished: (res) => {
+				updateImage({ image: res.url });
+			},
+			onFileUploadFailed: () => {
+				toast({
+					title: 'Error uploading your photo',
+					status: 'error',
+					isClosable: true,
+					position: 'bottom-right',
+				});
+			},
+		};
+
+		client.picker(options).open();
+	};
+
 	return (
 		<>
 			<Head>
 				<title>Dashboard Page</title>
 			</Head>
+			<UpdateUsernameModal
+				id={user.id}
+				isOpen={updateUsername.isOpen}
+				onClose={updateUsername.onClose}
+			/>
 			<DashboardLayout>
-				<Stack spacing="5">
-					<Text as="p">Address: {user.address}</Text>
-					<Button
-						onClick={() => signOut({ callbackUrl: '/signin' })}
-						variant="primary"
-					>
-						Sign out
-					</Button>
-				</Stack>
+				{isLoading ? (
+					<Center>
+						<Spinner />
+					</Center>
+				) : isError ? (
+					<Alert status="error">
+						<AlertIcon />
+						<AlertDescription>Error fetching profile</AlertDescription>
+					</Alert>
+				) : (
+					<Stack spacing="5">
+						<Stack alignItems="center" spacing={5} py={5}>
+							{profile?.image ? (
+								<Image
+									src={profile.image}
+									alt="profile image"
+									w="48"
+									borderRadius="full"
+								/>
+							) : (
+								<Image
+									src="/default_profile.png"
+									alt="profile image"
+									w="48"
+									borderRadius="full"
+								/>
+							)}
+							<Box>
+								<Button onClick={uploadProfileImage} size="xs">
+									Edit Profile Photo
+								</Button>
+							</Box>
+						</Stack>
+						<Stack>
+							<Text as="h3" size="xs">
+								Public Address
+							</Text>
+							<Text
+								as="p"
+								fontSize={{ base: '10px', sm: 'xs', md: 'sm' }}
+								fontWeight="bold"
+							>
+								{user.address}
+							</Text>
+						</Stack>
+						<Stack>
+							<HStack>
+								<Text as="h3" size="xs">
+									Username
+								</Text>
+								<IconButton
+									onClick={updateUsername.onOpen}
+									icon={<FiEdit />}
+									aria-label="edit username"
+									size="xs"
+								/>
+							</HStack>
+							<Text
+								as="p"
+								fontSize={{ base: '10px', sm: 'xs', md: 'sm' }}
+								fontWeight="bold"
+							>
+								{profile?.username || '-'}
+							</Text>
+						</Stack>
+						<Button
+							onClick={() => signOut({ callbackUrl: '/signin' })}
+							variant="primary"
+						>
+							Sign out
+						</Button>
+					</Stack>
+				)}{' '}
 			</DashboardLayout>
 		</>
 	);
